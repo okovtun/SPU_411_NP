f13da36c514aab52449e7190f90985e148abeae4
User <User@DESKTOP-QHG18FL>
2026-05-30 12:25:22 +0300

    Info DONE

--- IPcalculator/IPcalculator/main.cpp
+++ IPcalculator/IPcalculator/main.cpp
@@ -16,6 +16,8 @@ Prefix:
 */
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+CHAR* FormatAddress(CHAR szBuffer[], CONST CHAR szMessage[], DWORD dwIPaddress);
+CHAR* FormatCount(CHAR szBuffer[], CONST CHAR szMessage[], DWORD dwCount);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -29,7 +31,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_INITDIALOG:
 		SetFocus(GetDlgItem(hwnd, IDC_IPADDRESS));
-		SendMessage(GetDlgItem(hwnd, IDC_SPIN_PREFIX), UDM_SETRANGE, 0, MAKEWORD(32, 0));
+		SendMessage(GetDlgItem(hwnd, IDC_SPIN_PREFIX), UDM_SETRANGE, 0, MAKEWORD(30, 0));
 		break;
 	case WM_COMMAND:
 	{
@@ -72,15 +74,40 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			dwIPmask = UINT_MAX;
 			for (int i = 0; i < 32 - dwPrefix; i++)dwIPmask <<= 1;
 			SendMessage(hIPmask, IPM_SETADDRESS, 0, dwIPmask);
+			//break;
+		
+
+		case IDOK:
+		{
+			SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwIPaddress);
+			SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dwIPmask);
+			DWORD dwNetworkAddress = dwIPaddress & dwIPmask;
+			DWORD dwBroadcastAddress = dwIPaddress | ~dwIPmask;
+			DWORD dwIPsCount = dwBroadcastAddress - dwNetworkAddress + 1;
+			DWORD dwHostsCount = dwBroadcastAddress - dwNetworkAddress - 1;
+
+			CHAR szNetworkAddress[64] = {};
+			CHAR szBroadcastAddress[64] = {};
+			CHAR szIPsCount[64] = {};
+			CHAR szHostsCount[64] = {};
+			CHAR szInfo[256] = {};
+
+			sprintf
+			(
+				szInfo,"%s;\n%s;\n%s;\n%s;\n",
+				FormatAddress(szNetworkAddress, "Адрес сети:\t\t\t", dwNetworkAddress),
+				FormatAddress(szBroadcastAddress, "Широковещательный адрес:\t", dwBroadcastAddress),
+				FormatCount(szIPsCount, "Количество IP-адресов:", dwIPsCount),
+				FormatCount(szHostsCount, "Количество узлов:\t", dwHostsCount)
+			);
+			SendMessage(GetDlgItem(hwnd, IDC_STATIC_INFO), WM_SETTEXT, 0, (LPARAM)szInfo);
+		}
 			break;
+		case IDCANCEL:EndDialog(hwnd, 0);
 		case IDC_BUTTON_RESET:
 			SendMessage(hIPaddress, IPM_CLEARADDRESS, 0, 0);
 			SendMessage(hIPmask, IPM_CLEARADDRESS, 0, 0);
 			break;
-
-		case IDOK:
-			break;
-		case IDCANCEL:EndDialog(hwnd, 0);
 		}
 	}
 	break;
@@ -91,6 +118,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		DWORD dwIPmask = 0;
 		DWORD dwPrefix = 0;
 		CHAR szPrefix[3] = {};
+
 		SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dwIPmask);
 		for (dwPrefix = 0; dwIPmask; dwPrefix++)dwIPmask <<= 1;
 		sprintf(szPrefix, "%i", dwPrefix);
@@ -100,4 +128,23 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_CLOSE:EndDialog(hwnd, 0);
 	}
 	return FALSE;
+}
+
+CHAR* FormatAddress(CHAR szBuffer[], CONST CHAR szMessage[], DWORD dwIPaddress)
+{
+	sprintf
+	(
+		szBuffer, "%s%i.%i.%i.%i",
+		szMessage, 
+		FIRST_IPADDRESS(dwIPaddress),
+		SECOND_IPADDRESS(dwIPaddress),
+		THIRD_IPADDRESS(dwIPaddress),
+		FOURTH_IPADDRESS(dwIPaddress)
+	);
+	return szBuffer;
+}
+CHAR* FormatCount(CHAR szBuffer[], CONST CHAR szMessage[], DWORD dwCount)
+{
+	sprintf(szBuffer, "%s\t\t%i",szMessage, dwCount);
+	return szBuffer;
 }
